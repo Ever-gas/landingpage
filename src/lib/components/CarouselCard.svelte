c2c6348f91fcac77c80fbd40df3ca71ed3f91cc3
goldenhub <goldenazubuike@gmail.com>
2024-05-04 21:24:50 +0100

    carousel section

--- src/lib/components/CarouselCard.svelte
+++ src/lib/components/CarouselCard.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <div class="rounded-2xl group relative h-full cursor-pointer w-[85%] md:w-full">
-    <div class={`h-full w-full rounded-2xl group-hover:opacity-0 opacity-100 group-hover:z-0 z-50 transition-all duration-700 absolute top-0 left-0 bg-[url('${img}')] bg-cover bg-no-repeat bg-center`}>
+    <div class="h-full w-full rounded-2xl group-hover:opacity-0 opacity-100 group-hover:z-0 z-50 transition-all duration-700 absolute top-0 left-0 {img} bg-cover bg-no-repeat bg-center">
         <!-- <img class="object-cover h-full w-full" src="{img}" alt="" role="presentation" /> -->
     </div>
     <div class={`${color} px-[22px] lg:px-[26px] py-[32px] rounded-2xl flex flex-col justify-between h-full w-full z-0 group-hover:z-50 absolute`}>
