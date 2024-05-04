dc4322666d79b8088c88827a1a38f7edf8733792
goldenhub <goldenazubuike@gmail.com>
2024-05-04 20:38:36 +0100

    carousel section

--- src/lib/components/CarouselCard.svelte
+++ src/lib/components/CarouselCard.svelte
@@ -6,8 +6,10 @@
     export let img;
 </script>
 
-<div class="rounded-2xl group relative cursor-pointer w-[85%] md:w-full">
-    <img class="object-cover h-full w-full rounded-2xl group-hover:opacity-0 opacity-100 group-hover:z-0 z-50 transition-all duration-700 absolute top-0 left-0" src="{img}" alt="" role="presentation" />
+<div class="rounded-2xl group relative h-full cursor-pointer w-[85%] md:w-full">
+    <div class={`h-full w-full rounded-2xl group-hover:opacity-0 opacity-100 group-hover:z-0 z-50 transition-all duration-700 absolute top-0 left-0 bg-[url('${img}')] bg-cover bg-no-repeat bg-center`}>
+        <!-- <img class="object-cover h-full w-full" src="{img}" alt="" role="presentation" /> -->
+    </div>
     <div class={`${color} px-[22px] lg:px-[26px] py-[32px] rounded-2xl flex flex-col justify-between h-full w-full z-0 group-hover:z-50 absolute`}>
         <div class="mb-[60px]">
             <img src="/images/arrow-diagonal-black.svg" alt="" role="presentation" class="mb-8" />
